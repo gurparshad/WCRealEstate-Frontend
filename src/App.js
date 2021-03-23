92dd41dad9241f50efd8a1bdff68d467033a6516
gurparshad <panksharma230@gmail.com>
2021-03-23 13:36:09 +0200

    register and login component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import MenuBar from "./components/MenuBar/MenuBar";
 import Banner from "./components/Banner/Banner";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+
+export const authentication = {
+  isLoggedIn: false,
+
+  onAuthentication() {
+    this.isLoggedIn = true;
+  },
+
+  onLogout() {
+    this.isLoggedIn = false;
+  },
+
+  getLogInStatus() {
+    return this.isLoggedIn;
+  },
+};
+
+export const SecuredRoute = (props) => {
+  return (
+    <Route
+      path={props.path}
+      render={(data) =>
+        authentication.getLogInStatus() ? (
+          <props.component {...data}></props.component>
+        ) : (
+          <Redirect to={{ pathname: "/" }}></Redirect>
+        )
+      }
+    ></Route>
+  );
+};
 
 function App() {
   return (
-    <div className="app">
-      <MenuBar />
-      <Banner />
-    </div>
+    <Router>
+      <div className="app">
+        <MenuBar />
+        <Switch>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+
+        {/* <SecuredRoute path="/dashboard" component={Dashboard}></SecuredRoute> */}
+        {/* <SecuredRoute path="/community" component={Community}></SecuredRoute> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Register/Register.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div>regiseter</div>;
+};
+
+export default Register;
